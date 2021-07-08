3b8aa5639e06f9d501dff16af10eddac7e180640
Prakhar-Shukla05 <prakhar3109@gmail.com>
2021-07-09 02:02:30 +0530

    Refactored the code to use Project lombok

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,16 +1,12 @@
 package guru.springframework.controllers;
-import guru.springframework.domain.Category;
-import guru.springframework.domain.UnitOfMeasure;
-import guru.springframework.repositories.CategoryRepository;
-import guru.springframework.repositories.UnitOfMeasureRepository;
+
 import guru.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Optional;
-
-
+@Slf4j
 @Controller
 public class IndexController {
 /*
@@ -31,13 +27,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model){
-
-    /*    Optional<Category> categoryOptional= categoryRepository.findByDescription("American");
-        Optional<UnitOfMeasure> unitOfMeasureOptional= unitOfMeasureRepository.findByDescription("Teaspoon");
-        System.out.println("Category Id is "+ categoryOptional.get().getId());
-        System.out.println("UOM Id is "+ unitOfMeasureOptional.get().getId());
-
-     */
+        log.debug("Getting index page");
         model.addAttribute("recipes",recipeService.getRecipes());
         return "index";
     }
