0dd5ee73e4939b9f5d84c7aa2b8c704a9bbeee0c
Prakhar-Shukla05 <prakhar3109@gmail.com>
2021-07-09 02:59:48 +0530

    Added JUnit Test for Category ID

--- src/test/java/guru/springframework/Spring5RecipeAppApplicationTests.java
+++ src/test/java/guru/springframework/Spring5RecipeAppApplicationTests.java
@@ -6,7 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+//SpringRunner brings up the Spring Context
 @SpringBootTest
+//This is an integration test
 public class Spring5RecipeAppApplicationTests {
 
 	@Test
